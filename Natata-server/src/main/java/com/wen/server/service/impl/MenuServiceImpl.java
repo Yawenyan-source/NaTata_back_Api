33f7dcc95e01ef3605ccf27cc9729e5e1a84773d
wen <1785484015@qq.com>
2021-10-13 16:41:27 +0800

    redis中存取菜单数据

--- Natata-server/src/main/java/com/wen/server/service/impl/MenuServiceImpl.java
+++ Natata-server/src/main/java/com/wen/server/service/impl/MenuServiceImpl.java
@@ -1,14 +1,17 @@
 package com.wen.server.service.impl;
 
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.wen.server.entity.Admin;
 import com.wen.server.entity.Menu;
-import com.wen.server.mapper.AdminMapper;
 import com.wen.server.mapper.MenuMapper;
 import com.wen.server.service.IMenuService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import java.util.List;
 
@@ -25,9 +28,22 @@ public class MenuServiceImpl extends ServiceImpl<MenuMapper, Menu> implements IM
     @Autowired
     private MenuMapper menuMapper;
 
+    @Autowired
+    @Qualifier("myRedisTemplate")
+    private RedisTemplate<String, Object> redisTemplate;
 
     @Override
     public List<Menu> getMenuByAdminId() {
-        return menuMapper.getMenuByAdminId(((Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId());
+        Integer adminId = ((Admin) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId();
+        //从redis获取菜单数据
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        List<Menu> menus = (List<Menu>) valueOperations.get("menu_" + adminId);
+        //如果为空,从数据库中获取
+        if (CollectionUtils.isEmpty(menus)) {
+            menus = menuMapper.getMenuByAdminId(adminId);
+            //将数据返回到redis
+            valueOperations.set("menu_" + adminId, menus);
+        }
+        return menus;
     }
 }
