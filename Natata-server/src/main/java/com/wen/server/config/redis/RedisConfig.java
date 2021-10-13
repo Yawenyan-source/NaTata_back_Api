387a1865902ccf392f5e9c2b96fad78d30996293
wen <1785484015@qq.com>
2021-10-13 16:19:12 +0800

    引入redis依赖,配置redis

---
+++ Natata-server/src/main/java/com/wen/server/config/redis/RedisConfig.java
@@ -0,0 +1,35 @@
+package com.wen.server.config.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * redis配置类
+ *
+ * @author Wen
+ * @date 2021年10月13日 4:06 下午
+ */
+@Configuration
+public class RedisConfig {
+    /**
+     * 序列化配置
+     *
+     * @return
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        //string类型 key序列器
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        //string类型 value序列器
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        //hash类型 key序列器
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        //hash类型 value序列器
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+}
